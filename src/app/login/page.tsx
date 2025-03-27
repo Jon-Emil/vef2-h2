361ab9dc4201f56bfcc8c5679ccfb0550ba7b42f
Sigurður Ari Stefánsson <sigurdur.ari@gmail.com>
2025-03-27 21:43:04 +0000

    styling á forms

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,11 @@
 import Navigation from "@/components/Navigation/Navigation";
+import styles from "../page.module.css";
 import Login from "@/components/Login/Login";
 
 export default async function Home() {
 
   return (
-    <div>
+    <div className={styles.page}>
       <Navigation />
       <Login/>
     </div>
