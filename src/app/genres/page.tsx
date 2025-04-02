7c7c1bd9551473e49c87f6dc42c90323a680d30c
Sigurður Ari Stefánsson <sigurdur.ari@gmail.com>
2025-04-02 16:05:32 +0000

    eslint fix

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -1,6 +1,4 @@
-import Navigation from "@/components/Navigation/Navigation";
 import Genres from "@/components/Genres/Genres";
-import Footer from "@/components/Footer/Footer";
 
 export default async function Home() {
   return (
