3df2c7b6d9ff05e5e4e60f1cabf8c10ae5401779
Sigurður Ari Stefánsson <sigurdur.ari@gmail.com>
2025-04-02 15:57:12 +0000

    basic movie info and styles

--- src/components/DisplayedMovie/DisplayedMovie.tsx
+++ src/components/DisplayedMovie/DisplayedMovie.tsx
@@ -26,32 +26,37 @@ export default function Genres({ movieSlug }: { movieSlug: string }) {
       }
     }
     fetchData();
-  }, []);
+  }, [movieSlug]);
 
   return (
     <div>
       {uiState === "loading" && <p>Sæki mynd</p>}
       {uiState === "error" && <NotFound/>}
       {uiState === "data" && movie && (
-        <div>
-          <img
-            src={movie.img_url}
-            className={styles.Image}
-            alt={`movie poster for the movie: ${movie.title}`}
-          />
-          <div>
-            <h2>{movie.title}</h2>
-            <p>{`(${movie.year})`}</p>
-            <ul>
-              {movie.genres.map((genre, index) => (
-                <li key={index}>
-                  <Genre genre={genre} />
-                </li>
-              ))}
-            </ul>
+      <div className={styles.movie_container}>
+        <img
+          src={movie.img_url}
+          className={styles.movie_poster}
+          alt={`Movie poster for the movie: ${movie.title}`}
+        />
+        <div className={styles.movie_details}>
+          <h2 className={styles.movie_title}>{movie.title}</h2>
+          <p className={styles.movie_year}>({movie.year})</p>
+          <p className={styles.movie_director}>Directed by: {movie.director}</p>
+          <div className={styles.movie_description}>
+            <h3>Description:</h3>
+            <p>{movie.description}</p>
           </div>
+          <ul className={styles.movie_genres}>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>
+                <Genre genre={genre} />
+              </li>
+            ))}
+          </ul>
         </div>
-      )}
-    </div>
+      </div>
+      )}</div>
   );
+
 }
